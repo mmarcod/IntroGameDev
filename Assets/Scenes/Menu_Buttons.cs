dc5b347c52abcf304f0384165c466d9f603bfcf1
mmarcod <marco.davila001@mymdc.net>
2024-11-24 17:25:17 -0500

    Initial commit

---
+++ Assets/Scenes/Menu_Buttons.cs
@@ -0,0 +1,27 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Buttons : MonoBehaviour
+{
+    //method to use to load Scene1
+    public void LoadLab9_Terrain()
+    {
+        SceneManager.LoadScene("Lab9_Terrain);
+    }
+    public void LoadCarTimer()
+    {
+        SceneManager.LoadScene("CarTimer);
+    }
+    public void LoadExercise11()
+    {
+        SceneManager.LoadScene("Exercise11);
+    }
+    public void LoadMenuScene()
+    {
+        //loads the scene1 using the method calledLoad Scene
+        SceneManager.LoadScene("MenuScene");
+    }
+}
+*/
